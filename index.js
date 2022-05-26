a345607e08a19c6ce51d57c78f620e01d9a21026
Eva Reitzel <evareitzel@gmail.com>
2022-05-25 23:51:00 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+
+//  function findMatching(){}
+function findMatching(drivers, name){
+    return drivers.filter(driver => driver.toUpperCase() === name.toUpperCase());
+}
+
+// function fuzzyMatch(){}
+function fuzzyMatch(drivers, firstLetter){
+    return drivers.filter(driver => driver.charAt(0) === firstLetter.charAt(0));
+}
+
+// function matchName(){}
+function matchName(drivers, string){
+    return drivers.filter(driver => driver.name === string);
+}
